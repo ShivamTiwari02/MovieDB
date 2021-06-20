3c9218f36a5f78532979f4d5ebc97a6a422f73d4
Shivam Tiwari <shieldcap2702@gmail.com>
2021-06-20 15:58:31 +0530

    pata nahi

--- src/Component/Movies/Movie/Movie.js
+++ src/Component/Movies/Movie/Movie.js
@@ -10,7 +10,8 @@ class Movie extends Component{
     state = {
         title: '',
         desc: '',
-        img: 'https://image.tmdb.org/t/p/w500/'
+        img: 'https://image.tmdb.org/t/p/w500/',
+        Top_10: ['']
     } 
 
     componentDidMount(){
@@ -20,7 +21,7 @@ class Movie extends Component{
             this.setState({title : res.data.original_title});
             this.setState({desc : res.data.overview});
             this.setState({img : this.state.img + res.data.poster_path});
-            console.log(this.state.img)
+            // console.log(this.state.img)
         })
         .catch(err => {
             console.log(err);                
@@ -28,17 +29,47 @@ class Movie extends Component{
     }
 
     topMoviesHandler(){
-        
+        axios.get('https://api.themoviedb.org/3/movie/top_rated?api_key=c9591b3aea2e8872d00c222ef4d74bd1&language=en-US&page=1')
+        .then(res =>{
+            const titles = [];
+            res.data.results.map(result => {
+                // console.log(result.original_title);
+                titles.push(result.original_title);
+            });
+            this.setState({Top_10: titles})
+            // console.log(this.state.Top_10)
+        })
+        .catch(err => {
+            console.log(err);                
+        });
     }
 
     render(){
+
+        let top10 = null;
+        if(this.state.Top_10.length > 5){
+            top10 = <ul>{this.state.Top_10.map(
+                movie => {
+                    return(
+                    <li>{movie}</li>
+                    // console.log(movie + "   1")
+                    )
+                }
+            )
+            }</ul>
+        };
+
+        console.log(top10)
+
+
         return(
             <div className='Movie'>
                 <div className="movie_card" >
                     <h3>{this.state.title}</h3>
                     <img src = {this.state.img} />
                     <p>{this.state.desc.substr(0,100)}.... <a >read more</a></p>
-                    <button onClick = {this.topMoviesHandler}>Top Movies</button>
+                    <button onClick = {() => this.topMoviesHandler()}>Top Movies</button>
+                    {top10}
                 </div>
             </div>
         )
